8e83490c756e8ea87a9fcb3d7dd7fe4e508d9114
DESKTOP-5DTE3VI\nguye <nguyenthanhphuong272@gmail.com>
2017-07-28 14:17:49 +0700

    init

---
+++ spec/my-atom-chat-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MyAtomChatView from '../lib/my-atom-chat-view';
+
+describe('MyAtomChatView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
